948f3b199aff83c38f20670ef2666e004b9dea4f
Mr. Sadsouler <siddh.mistry396321@gmail.com>
2019-09-16 16:00:46 +0530

    Updated

--- app/src/main/java/tk/hackeridiot/chat/FindFriendsActivity.java
+++ app/src/main/java/tk/hackeridiot/chat/FindFriendsActivity.java
@@ -17,21 +17,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
-import java.sql.DatabaseMetaData;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
-    private RecyclerView FindFriendsRecyclerView;
+    private RecyclerView FindFriendsRecyclerList;
     private DatabaseReference UsersRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
-        FindFriendsRecyclerView = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
-        FindFriendsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
@@ -50,18 +49,18 @@ public class FindFriendsActivity extends AppCompatActivity {
                     protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model) {
                         holder.userName.setText(model.getName());
                         holder.userStatus.setText(model.getStatus());
-                        Picasso.get().load(model.getImage()).into(holder.profileImage);
+                        Picasso.get().load(model.getImage()).placeholder(R.drawable.logo_app).into(holder.profileImage);
                     }
 
                     @NonNull
                     @Override
-                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
                         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout, viewGroup, false);
                         FindFriendViewHolder viewHolder = new FindFriendViewHolder(view);
                         return viewHolder;
                     }
                 };
-        FindFriendsRecyclerView.setAdapter(adapter);
+        FindFriendsRecyclerList.setAdapter(adapter);
         adapter.startListening();
     }
     public static class FindFriendViewHolder extends RecyclerView.ViewHolder{
@@ -70,7 +69,7 @@ public class FindFriendsActivity extends AppCompatActivity {
         public FindFriendViewHolder(@NonNull View itemView) {
             super(itemView);
             userName = itemView.findViewById(R.id.user_profile_name);
-            userName = itemView.findViewById(R.id.user_status);
+            userStatus = itemView.findViewById(R.id.user_status);
             profileImage = itemView.findViewById(R.id.users_profile_image);
         }
     }
